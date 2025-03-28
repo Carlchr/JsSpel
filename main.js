496fb060bc17668dcbcfa2570907e8456ba719e2
Carlchr <Elias.c.christensson@gmail.com>
2025-03-28 09:57:16 +0100

    Start på projektet

---
+++ main.js
@@ -0,0 +1,14 @@
+//Player
+let player_xPos = 500;
+let player_yPos = 500;
+const player_size = 25;
+const player_speed = 5;
+
+//Kollar om knappen är nedtryckt
+//Lägga till arrow keys
+let keys = {
+  w: false,
+  a: false,
+  s: false,
+  d: false,
+};
