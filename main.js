83c7cbf0b49ea3e19cd33494c43c991c6b58f012
Carlchr <Elias.c.christensson@gmail.com>
2025-03-28 09:57:50 +0100

    Test

--- main.js
+++ main.js
@@ -12,3 +12,20 @@ let keys = {
   s: false,
   d: false,
 };
+
+//Kollar om man trycker ner knappen
+document.onkeydown = function (e) {
+  console.log(e); //Inparametern e innehåller ett event-objekt med information om eventet.
+  const key = e.key;
+
+  //Objektet får värdet true
+  keys[key] = true;
+};
+
+//Kollar om knappen är inte tryckt
+document.onkeyup = function (e) {
+  const key = e.key;
+
+  //Objektet fär värde false
+  keys[key] = false;
+};
